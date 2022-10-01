a983e4507695ac13acee07dc399d821624e4aed2
Joseph <josephrock225@gmail.com>
2022-09-30 20:33:14 -0700

    Slow progress. Need to figure out how to parse what I want

--- wikicrawler.py
+++ wikicrawler.py
@@ -1,21 +1,29 @@
 import xml.etree.ElementTree as etree
-import codecs
-import csv
-import time
 import os
-import sys
 
-PATH_WIKI_XML = 'D:/newest/'
+PATH_WIKI_XML = '/mnt/d/newest'
 FILENAME_WIKI = 'enwiki-latest-pages-articles-multistream.xml'
 FILENAME_ARTICLES = 'articles.csv'
 FILENAME_REDIRECT = 'articles_redirect.csv'
 FILENAME_TEMPLATE = 'articles_template.csv'
 ENCODING = "utf-8"
 
-
-print(os.path)
 pathWikiXML = os.path.join(PATH_WIKI_XML, FILENAME_WIKI)
 
+def strip_tag_name(t):
+    idx = t.rfind("}")
+    if idx != -1:
+        t = t[idx + 1:]
+    return t
+
+count = 0
 for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
-    print(elem)
-    break
+    tname = strip_tag_name(elem.tag)
+
+    print(tname, elem.text)
+
+    elem.clear()
+
+    count += 1
+    if count == 1000:
+        break
