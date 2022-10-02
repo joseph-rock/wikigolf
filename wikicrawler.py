08d464179ac204e86e51e24ddb323e5a20bbbe0c
Joseph <josephrock225@gmail.com>
2022-10-02 16:24:02 -0700

    Before first test

--- wikicrawler.py
+++ wikicrawler.py
@@ -3,47 +3,46 @@ import re
 import os
 import sqlite3
 
-con = sqlite3.connect("wikilinks.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS pages(page, link)")
-
-PATH_WIKI_XML = '/mnt/d/newest'
-FILENAME_WIKI = 'enwiki-latest-pages-articles-multistream.xml'
-
-pathWikiXML = os.path.join(PATH_WIKI_XML, FILENAME_WIKI)
-
 def strip_tag_name(t):
     idx = t.rfind("}")
     if idx != -1:
         t = t[idx + 1:]
     return t
 
-count = 0
-redirect = False
-for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
-    tname = strip_tag_name(elem.tag)
 
-    if event == 'start':
-        if tname == "title":
-            title = elem.text
-        if tname == "redirect":
-            redirect = True
+def main():
+    con = sqlite3.connect("wikilinks.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS pages(page, link)")
 
-    elif event == 'end':
-        if tname == 'text' and redirect:
-            redirect = False
-            continue
-        elif tname == 'text' and not redirect:
-            links = re.findall('(?:\[{2})([\w\s]+)(?:[|\w\s]*)(?:\]{2})', elem.text)
+    PATH_WIKI_XML = '/mnt/d/newest'
+    FILENAME_WIKI = 'enwiki-latest-pages-articles-multistream.xml'
+    pathWikiXML = os.path.join(PATH_WIKI_XML, FILENAME_WIKI)
 
-            for link in links:
-                cur.execute("INSERT INTO pages VALUES(?, ?)", (title, link))
-                
+    count = 0
+    redirect = False
+    for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
+        tname = strip_tag_name(elem.tag)
 
-    count += 1
-    if count == 1000000:
-        print(count)
+        if event == 'start':
+            if tname == "title":
+                title = elem.text
+            if tname == "redirect":
+                redirect = True
 
+        elif event == 'end':
+            if tname == 'text' and redirect:
+                redirect = False
+                continue
+            elif tname == 'text' and not redirect:
+                links = re.findall('(?:\[{2})([\w\s]+)(?:[|\w\s]*)(?:\]{2})', elem.text)
 
-    con.commit()
+                for link in links:
+                    cur.execute("INSERT INTO pages VALUES(?, ?)", (title, link))          
+
+        count += 1
+        if count == 1000000:
+            print(count)
+        con.commit()
+
 
