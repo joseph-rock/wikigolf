f14e9a59ea3c2870a1b3cfe0b041027be4459b10
Joseph <josephrock225@gmail.com>
2022-10-02 19:01:46 -0700

    Before 2nd try

--- sqlitetest.py
+++ sqlitetest.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-con = sqlite3.connect("wikilinks.db")
+con = sqlite3.connect("/mnt/d/wikilinks.db")
 cur = con.cursor()
 # cur.execute("CREATE TABLE IF NOT EXISTS pages(page, link)")
 
-res = cur.execute("SELECT * FROM pages")
+res = cur.execute("SELECT * FROM pages WHERE page = 'Cannibal Corpse'")
 print(res.fetchall())
--- wikicrawler.py
+++ wikicrawler.py
@@ -11,7 +11,7 @@ def strip_tag_name(t):
 
 
 def main():
-    con = sqlite3.connect("wikilinks.db")
+    con = sqlite3.connect("/mnt/d/wikilinks.db")
     cur = con.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS pages(page, link)")
 
@@ -19,7 +19,8 @@ def main():
     FILENAME_WIKI = 'enwiki-latest-pages-articles-multistream.xml'
     pathWikiXML = os.path.join(PATH_WIKI_XML, FILENAME_WIKI)
     redirect = False
-    
+
+    count = 0
     for event, elem in etree.iterparse(pathWikiXML, events=('start', 'end')):
         tname = strip_tag_name(elem.tag)
 
@@ -34,11 +35,28 @@ def main():
                 redirect = False
                 continue
             elif tname == 'text' and not redirect:
-                links = re.findall('(?:\[{2})([\w\s]+)(?:[|\w\s]*)(?:\]{2})', elem.text)
-                for link in links:
-                    cur.execute("INSERT INTO pages VALUES(?, ?)", (title, link))          
-
-        con.commit()
+                try:
+                    links = re.findall('(?:\[{2})([\w\s]+)(?:[|\w\s]*)(?:\]{2})', elem.text)
+                    block = []
+                    for link in links:
+                        block.append((title, link))
+                        
+                    cur.executemany("INSERT INTO pages VALUES(?, ?)", block)          
+                    con.commit()
+                except TypeError:
+                    print(title)
+        count += 1
+        if count > 100000:
+            print(count)
+            count = 0
+            con.commit
+            con.close()
+            con = sqlite3.connect("/mnt/d/wikilinks.db")
+            cur = con.cursor()
+    
+    con.commit
+    con.close()
+            
 
 
 main()
