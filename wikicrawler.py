e55e567e7088a45c8070b9ae946e04b14b7721c1
Joseph <josephrock225@gmail.com>
2022-10-09 18:18:39 -0700

    Refactor

--- wikicrawler.py
+++ wikicrawler.py
@@ -1,7 +1,9 @@
+from distutils.log import error
 from multiprocessing import Queue, Process
 import xml.etree.ElementTree as etree
 import re
 import os
+import time
 import sqlite3
 import logging
 import progressbar
@@ -14,36 +16,55 @@ def strip_tag_name(t):
     return t
 
 
-def sql_worker(q):
-    con = sqlite3.connect("/mnt/d/wikilinks.db")
-    cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS pages(page, link)")
+def startLog(pid, description):
+    logging.info(f"pid:{pid} {description} started")
 
-    logging.error(f"SQL {os.getpid()} started")
-    while True:
-        block = q.get()
 
-        if block is None:
-            break
+def endLog(pid, description):
+    logging.info(f"pid:{pid} {description} finished")
 
-        cur.executemany("INSERT INTO pages VALUES(?, ?)", block)          
-        con.commit()
 
-    con.close()
-    logging.error(f"SQL {os.getpid()} finished")
+def errorLog(pid, description, title=None, text=None):
+    if title == None:
+        logging.error(f'pid:{pid} {description}')
+    else:
+        logging.error(f'pid:{pid} {description}\n\tTitle: {title}\n\tText:  {text}')
+
+
+def xml_worker(regQ):
+    startLog(os.getpid(), "XML")
+
+    bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
+    num_pages = 0
+    for _, elem in etree.iterparse(PATH_WIKI_XML):
+        tname = strip_tag_name(elem.tag)
+
+        if tname == 'title':
+            title = elem.text
+        elif tname == 'text':
+            text = elem.text
+            regQ.put((title, text))
+            bar.update(num_pages)
+            num_pages += 1
+                    
+        elem.clear()
+        
+    for _ in range(NUM_REGEX_PROC):
+        regQ.put(None)
+
+    endLog(os.getpid(), "XML")
 
 
 def reg_worker(regQ, sqlQ):
-    REGEX = r'(?:\[\[)([^\[\]]+?)(?:\|[^\[\]]*)?(?:\]\])'
-    block = []
+    startLog(os.getpid(), "Regex")
 
-    logging.error(f"Regex {os.getpid()} started")
+    block = []
     while True:
         try:
             title, text = regQ.get()
 
         except TypeError:
-            logging.error(f"Regex {os.getpid()} found type None")
+            errorLog(os.getpid(), "Regex - Found 'None'")
             break
 
         try: 
@@ -58,75 +79,81 @@ def reg_worker(regQ, sqlQ):
                 block = []
                 
         except TypeError:
-            logging.error(f'Regex {os.getpid()} failed on: {title}\n{text}')
+            errorLog(os.getpid(), "Regex - Failed", title, text)
 
         except:
-            logging.error(f"Regex {os.getpid()} something else happened: {title}\n{text}")
+            errorLog(os.getpid(), "Regex - Other", title, text)
 
     if len(block) > 0:
         sqlQ.put(block)
+    time.sleep(10)
     sqlQ.put(None)
-    logging.error(f"Regex {os.getpid()} finished")
+
+    endLog(os.getpid(), "Regex")
 
 
-def xml_worker(regQ):
-    PATH_WIKI_XML = '/mnt/d/newest'
-    FILENAME_WIKI = 'enwiki-latest-pages-articles-multistream.xml'
-    PATH = os.path.join(PATH_WIKI_XML, FILENAME_WIKI)
+def sql_worker(q):
+    startLog(os.getpid(), "SQL")
 
-    bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength)
-    i = 0
-    
-    logging.error(f"XML {os.getpid()} started")
-    
-    for _, elem in etree.iterparse(PATH):
-        tname = strip_tag_name(elem.tag)
+    con = sqlite3.connect(PATH_DB)
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS pages(page, link)")
 
-        if tname == 'title':
-            title = elem.text
-        elif tname == 'text':
-            text = elem.text
-            regQ.put((title, text))
-            bar.update(i)
-            i += 1
-                    
-        elem.clear()
-        
-    regQ.put(None)
-    logging.error(f"XML {os.getpid()} finished")
+    count = 0
+    while True:
+        block = q.get()
+
+        if block is None:
+            break
+
+        cur.executemany("INSERT INTO pages VALUES(?, ?)", block)
+        count += 1
+        if count > 100:       
+            con.commit()
+            count = 0
+
+    con.commit()
+    con.close()
+    endLog(os.getpid(), "SQL")   
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename="info.log", filemode='a', format='%(asctime)s %(message)s')
-    logging.error(f"Main {os.getpid()} started")
+    PATH_WIKI_XML = '/mnt/d/newest/enwiki-latest-pages-articles-multistream.xml'
+    PATH_DB = '/mnt/d/wikilinks.db'
+    REGEX = r'(?:\[\[)([^\[\]]+?)(?:\|[^\[\]]*)?(?:\]\])'
 
-    sqlQ = Queue(10000)
-    regQ = Queue(10000)
+    NUM_REGEX_PROC = 4
+    QUEUE_SIZE = 10000
+
+    logging.basicConfig(
+        filename="info.log", 
+        filemode='a', 
+        format='%(asctime)s %(message)s', 
+        level=20)
+    startLog(os.getpid(), "Main")
+
+    sqlQ = Queue(QUEUE_SIZE)
+    regQ = Queue(QUEUE_SIZE)
 
     xmlP = Process(target=xml_worker, args=(regQ,))
-    regP1 = Process(target=reg_worker, args=(regQ, sqlQ,))
-    regP2 = Process(target=reg_worker, args=(regQ, sqlQ,))
-    regP3 = Process(target=reg_worker, args=(regQ, sqlQ,))
-    regP4 = Process(target=reg_worker, args=(regQ, sqlQ,))
+    regP = []
+    for i in range(NUM_REGEX_PROC):
+        p = Process(target=reg_worker, args=(regQ, sqlQ,))
+        regP.append(p)
     sqlP = Process(target=sql_worker, args=(sqlQ,))
     
     xmlP.start()
-    regP1.start()
-    regP2.start()
-    regP3.start()
-    regP4.start()
+    for p in regP:
+        p.start()
     sqlP.start()
 
     sqlQ.close()
-    regQ.close()
     sqlQ.join_thread()
+    regQ.close()
     regQ.join_thread()
 
-    xmlP.join()
-    regP1.join()
-    regP2.join()
-    regP3.join()
-    regP4.join()
+    xmlP.join()    
+    for p in regP:
+        p.join()
     sqlP.join()
-    logging.error(f"Main {os.getpid()} finished")
-    
+    endLog(os.getpid(), "Main")
